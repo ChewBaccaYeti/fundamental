91ba491ccbc156ec0573e7d230cb91a1ba4fb66b
Venedykt <chewbaccayeti@gmail.com>
2023-04-15 13:23:26 +0300

    project build

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,71 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: {
+        'postcss-import': {},
+        'tailwindcss/nesting': 'postcss-nesting',
+
+        'tailwindcss/custom-forms': {},
+        'tailwindcss/aspect-ratio': {},
+        'tailwindcss/placeholders': {},
+        'tailwindcss/text-shadow': {},
+        'tailwindcss/gradients': {},
+        'tailwindcss/transitions': {},
+        'tailwindcss/transforms': {},
+        'tailwindcss/utilities': {},
+        'tailwindcss/variants': {},
+        'tailwindcss/screens': {},
+
+        'tailwindcss/cursor': {},
+        'tailwindcss/display': {},
+        'tailwindcss/flex': {},
+        'tailwindcss/float': {},
+        
+
+        'tailwindcss/font-display': {},
+        'tailwindcss/font-variant': {},
+        'tailwindcss/font-weights': {},
+        'tailwindcss/font-stacks': {},
+        'tailwindcss/font-sizes': {},
+        'tailwindcss/font-family': {},
+        'tailwindcss/font-size': {},
+        'tailwindcss/font-style': {},
+        'tailwindcss/font-weight': {},
+
+        'tailwindcss/line-height': {},
+        'tailwindcss/letter-spacing': {},
+        'tailwindcss/list-styles': {},
+
+        'tailwindcss/border-collapse': {},
+        'tailwindcss/border-color': {},
+        'tailwindcss/border-width': {},
+        'tailwindcss/border-style': {},
+        'tailwindcss/border-radius': {},
+        
+        'tailwindcss/grid': {},
+        'tailwindcss/justify-content': {},
+        'tailwindcss/margin': {},
+        'tailwindcss/opacity': {},
+        'tailwindcss/outline': {},
+        'tailwindcss/padding': {},
+        'tailwindcss/z-index': {},
+
+        'tailwindcss/height': {},
+        'tailwindcss/width': {},
+        'tailwindcss/max-height': {},
+        'tailwindcss/max-width': {},
+        'tailwindcss/min-height': {},
+        'tailwindcss/min-width': {},
+
+        'tailwindcss/text-align': {},
+        'tailwindcss/text-decoration': {},
+        'tailwindcss/text-transform': {},
+
+
+        'tailwindcss/table-layout': {},
+        'tailwindcss/vertical-align': {},
+        'tailwindcss/whitespace': {},
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
