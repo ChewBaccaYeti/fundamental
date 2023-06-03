e2bf2f390b01845e9206a5b2c62bde1a66237678
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-06-03 17:37:41 +0300

    Update

--- src/scripts/toggleDropDown.js
+++ src/scripts/toggleDropDown.js
@@ -1,44 +1,36 @@
-const elements = document.querySelectorAll('.toggle-list_item')
+const toggle = () => {
+	const toggleItems = document.querySelectorAll('.toggle-list_item')
 
-for (const element of elements) {
-	element.addEventListener('click', function () {
-		if (!this.dataset.clicked) {
-			this.setAttribute('data-clicked', 'true')
-			this.style.opacity = '1'
-			this.style.backgroundColor = '#b0d5f7'
-			this.style.height = '240px'
-			this.style.padding = '140px'
-		} else {
-			this.removeAttribute('data-clicked')
-			this.removeAttribute('style')
-		}
-	})
+	toggleItems.forEach((item) => {
+		const button = item.querySelector('button')
+		const vector = item.querySelector('.vector')
 
-	const paragraphs = element.querySelectorAll('.toggle-paragraph')
+		item.addEventListener('click', () => {
+			const toggleTextElements = item.querySelectorAll('.toggle-text')
+			const toggleContent = item.querySelector('.toggle-content')
 
-	for (const paragraph of paragraphs) {
-		paragraph.addEventListener('click', function () {
-			if (!this.dataset.clicked) {
-				this.setAttribute('data-clicked', 'true')
-				this.style.opacity = '1'
-				this.style.fontSize = '18px'
-			} else {
-				this.removeAttribute('data-clicked')
-				this.removeAttribute('style')
-			}
+			toggleTextElements.forEach((textElement) => {
+				textElement.classList.toggle('active')
+				animateElement(textElement)
+			})
+
+			toggleContent.classList.toggle('expanded')
+			vector.classList.toggle('rotate')
 		})
-	}
+
+		item.addEventListener('mouseover', () => {
+			vector.classList.add('hover')
+		})
+
+		item.addEventListener('mouseout', () => {
+			vector.classList.remove('hover')
+		})
+	})
 }
 
-function toggle(element) {
-	const parent = element.parentNode
-	const content = parent.querySelector('.toggle-paragraph_content')
-
-	if (!content.dataset.clicked) {
-		content.setAttribute('data-clicked', 'true')
-		content.style.paddingTop = '32px'
-	} else {
-		content.removeAttribute('data-clicked')
-		content.removeAttribute('style')
-	}
+const animateElement = (element) => {
+	element.style.transition = 'opacity 0.3s ease'
+	element.style.opacity = element.classList.contains('active') ? '1' : '0'
 }
+
+toggle()
